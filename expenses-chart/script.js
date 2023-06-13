102e2cbd360e207bc2e72c6fbdea2a70b7bc2529
szymon-skalmierski <105440298+szymon-skalmierski@users.noreply.github.com>
2023-06-13 03:09:02 +0200

    expenses-chart

---
+++ expenses-chart/script.js
@@ -0,0 +1,27 @@
+import data from './data.json' assert {type: 'json'};
+
+const chartsContainer = document.querySelector('.chart-top-bars');
+const today = new Date().getDay()-1;
+
+for (let i in data) {
+    const newDiv = document.createElement("div");
+
+    const day = document.createElement("span");
+    day.appendChild(document.createTextNode(data[i].day))
+    
+    
+    const bar = document.createElement("div");
+    bar.classList.add('bar');
+    bar.style.height = `${data[i].amount*2*0.7}%`;
+    if(parseInt(i) === today) bar.style.backgroundColor = 'hsl(186, 34%, 60%)';
+
+    const amount = document.createElement('div');
+    amount.classList.add('amount');
+    amount.appendChild(document.createTextNode(`$${data[i].amount}`));
+
+    newDiv.appendChild(day);
+    newDiv.appendChild(bar);
+    newDiv.appendChild(amount);
+
+    chartsContainer.appendChild(newDiv);
+}
