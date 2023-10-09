7232d6b1aadfe4e4d55d3df3b81e7b2e98bb99ca
szymon-skalmierski <xketris@gmail.com>
2023-10-10 01:59:05 +0200

    layout migration

--- space-tourism-site/src/app/app-routing.module.ts
+++ space-tourism-site/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   {
     path: 'home',
-    component: AppComponent,
-  }, 
+    component: HomepageComponent,
+  },
   {
-    path: 'crew',
-  }
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
 ];
 
 @NgModule({
--- space-tourism-site/src/app/app.module.ts
+++ space-tourism-site/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrewComponent } from './crew/crew.component';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CrewComponent
+    CrewComponent,
+    HeaderComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
