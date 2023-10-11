99aedc0bc7384706a2e6f46d5481f9f447eb5941
szymon-skalmierski <xketris@gmail.com>
2023-10-11 04:25:46 +0200

    nav routing

--- space-tourism-site/src/app/app-routing.module.ts
+++ space-tourism-site/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { DestinationComponent } from './destination/destination.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomepageComponent,
+  },
+  {
+    path: 'destination',
+    component: DestinationComponent,
+  },
+  {
+    path: 'crew',
+    component: CrewComponent,
+  },
+  {
+    path: 'technology',
+    component: TechnologyComponent,
   },
   {
     path: '',
