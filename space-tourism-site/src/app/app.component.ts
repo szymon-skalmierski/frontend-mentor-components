1a2e00c48a8bf9b46eaab2bf08b4a02abfef59dc
szymon-skalmierski <xketris@gmail.com>
2023-10-16 16:52:43 +0200

    background images preloading

--- space-tourism-site/src/app/app.component.ts
+++ space-tourism-site/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'space-tourism-site';
+  imgs: any[] = [];
+
+  ngOnInit() {
+    this.pload(
+      "/assets/crew/background-crew-desktop.jpg",
+      "/assets/home/background-home-desktop.jpg",
+      "/assets/technology/background-technology-desktop.jpg",
+      "/assets/destination/background-destination-desktop.jpg",
+      );
+  }
+
+  pload(...args: any[]):void {
+    for(let imgPath of args) {
+      const img = new Image();
+      img.src = imgPath;
+      this.imgs.push(img);
+    }
+  }
 }
