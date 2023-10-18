3a6f6f0a1b5fdea0c93c8dd3d6b635f19276bd93
szymon-skalmierski <xketris@gmail.com>
2023-10-18 19:13:43 +0200

    destination navbar

--- space-tourism-site/src/app/destination/destination-routing.module.ts
+++ space-tourism-site/src/app/destination/destination-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import * as data from '../../../data.json';
+import * as dataJSON from '../../../data.json';
 import { DestinationComponent } from './destination.component';
 import { DestinationTargetComponent } from './destination-target/destination-target.component';
 
-const crewData = data;
+const data = dataJSON;
 
 const routes: Routes = [
   {
@@ -18,7 +18,23 @@ const routes: Routes = [
       },
       {
         path: 'Moon',
-        component: DestinationTargetComponent
+        component: DestinationTargetComponent,
+        data: data.destinations[0]
+      },
+      {
+        path: 'Mars',
+        component: DestinationTargetComponent,
+        data: data.destinations[1]
+      },
+      {
+        path: 'Europe',
+        component: DestinationTargetComponent,
+        data: data.destinations[2]
+      },
+      {
+        path: 'Titan',
+        component: DestinationTargetComponent,
+        data: data.destinations[3]
       },
     ]
   },
--- space-tourism-site/src/app/destination/destination-target/destination-target.component.ts
+++ space-tourism-site/src/app/destination/destination-target/destination-target.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-destination-target',
   templateUrl: './destination-target.component.html',
   styleUrls: ['./destination-target.component.scss']
 })
-export class DestinationTargetComponent {
+export class DestinationTargetComponent implements OnInit {
+  destinationTargetData: any;
 
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit() {
+    this.route.data.subscribe(data=>{this.destinationTargetData=data; console.log(this.destinationTargetData)});
+  }
 }
