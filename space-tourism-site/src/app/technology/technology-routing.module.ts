4adddc8ba8416a9e2418a9515ba5f11627b21531
szymon-skalmierski <xketris@gmail.com>
2023-10-20 21:19:57 +0200

    technology solution routing

--- space-tourism-site/src/app/technology/technology-routing.module.ts
+++ space-tourism-site/src/app/technology/technology-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
         pathMatch: 'full'
       },
       {
-        path: 'Spaceport',
+        path: 'SpacePort',
         component: TechnologySolutionComponent
       },
       {
