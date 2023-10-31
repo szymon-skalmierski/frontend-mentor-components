ec3b78a06a74d9576e86d655d0847e8d5a29f557
szymon-skalmierski <xketris@gmail.com>
2023-10-31 23:58:10 +0100

    expanded navbar

--- space-tourism-site/src/app/header/header.component.ts
+++ space-tourism-site/src/app/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isActive = false;
 
+  changeNavState() {
+    this.isActive = !this.isActive;
+  }
 }
